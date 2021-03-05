e8a2a6f7f4a9154d4d8aa0a8d839b217a757469b
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-03-05 20:00:38 +0200

    HW6_NodeJS

--- NodeJS_HW(6)/controller/user.controller.js
+++ NodeJS_HW(6)/controller/user.controller.js
@@ -39,17 +39,6 @@ module.exports = {
         }
     },
 
-    getByName: async (req, res) => {
-        try {
-            const { email } = req.body;
-            const user = await userService.findUserByEmail(email);
-
-            res.json(user);
-        } catch (e) {
-            res.status(errorCodesEnum.BAD_REQUEST).json(e.message);
-        }
-    },
-
     deleteUserById: async (req, res) => {
         try {
             const { userId } = req.params;
