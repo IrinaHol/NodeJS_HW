439fb62c1c2b061b43dd07ab8c8e142a44006402
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-03-31 23:15:40 +0300

    HW_NodeJS

--- HW(12)/app.js
+++ HW(12)/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
 
 dotenv.config({ path: path.join(process.cwd(), '../.env') });
+const swaggerDoc = require('./docs/swagger.json');
 
 const db = require('./dataBase').getInstance();
 const cron = require('./cron_Jobs');
@@ -24,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(process.cwd(), 'static')));
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/', apiRouter);
 
 // eslint-disable-next-line no-unused-vars
